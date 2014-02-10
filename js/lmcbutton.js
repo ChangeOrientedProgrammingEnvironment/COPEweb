5f23cfa5b2efc7064cb8bbb8aab742db071fb2ab
altern <sergey.shmarkatyuk@gmail.com>
2014-02-09 19:04:37 -0800

    fixed problem with empty html element id

--- js/lmcbutton.js
+++ js/lmcbutton.js
@@ -9,27 +9,29 @@ function isNotEmpty(str) {
 
 function ShowLMCButton(cliptext, capt, js, furl, htmlElemId)
 {
- var params = 'txt=' + encodeURIComponent(cliptext); 
- if (!isNotEmpty(furl)) { furl = "lmcbutton.swf"; }
- if (isNotEmpty(capt)) { params += '&capt=' + capt; }
- if (isNotEmpty(js)) { params += '&js=' + js; }
- 
-if(!htmlElemId) {
- document.write('<object width="40" height="20">');
- document.write(' <param name="movie" value="' + furl + '">');
- document.write(' <PARAM NAME=FlashVars VALUE="' + params + '">');
- document.write(' <embed src="' + furl + '" flashvars="' + params + '"  width="40" height="20"></embed>');
- document.write('</object>');
-} else {
- var html = '<object width="40" height="20">';
- html += ' <param name="movie" value="' + furl + '">';
- html += ' <PARAM NAME=FlashVars VALUE="' + params + '">';
- html += ' <embed src="' + furl + '" flashvars="' + params + '"  width="40" height="20"></embed>';
- html += '</object>';
- 
- var htmlElement = document.getElementById(htmlElemId);
- htmlElement.innerHTML = html;
-}
+  var params = 'txt=' + encodeURIComponent(cliptext); 
+  if (!isNotEmpty(furl)) { furl = "lmcbutton.swf"; }
+  if (isNotEmpty(capt)) { params += '&capt=' + capt; }
+  if (isNotEmpty(js)) { params += '&js=' + js; }
+  
+ if(!htmlElemId) {
+  document.write('<object width="40" height="20">');
+  document.write(' <param name="movie" value="' + furl + '">');
+  document.write(' <PARAM NAME=FlashVars VALUE="' + params + '">');
+  document.write(' <embed src="' + furl + '" flashvars="' + params + '"  width="40" height="20"></embed>');
+  document.write('</object>');
+ } else {
+  var html = '<object width="40" height="20">';
+  html += ' <param name="movie" value="' + furl + '">';
+  html += ' <PARAM NAME=FlashVars VALUE="' + params + '">';
+  html += ' <embed src="' + furl + '" flashvars="' + params + '"  width="40" height="20"></embed>';
+  html += '</object>';
+  
+  var htmlElement = document.getElementById(htmlElemId);
+  if(htmlElement) {
+    htmlElement.innerHTML = html;
+  }
+ }
 
 //alert('file: ' + furl + ' Params: ' + params); // debug
 }
